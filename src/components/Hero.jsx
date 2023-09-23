78b292d868842963a3a5fc9656d500a9acce29f4
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-23 21:44:21 +0600

    hero section complite

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const Hero() {
-  return (
-    <div>
-      
-    </div>
-  )
+function Hero() {
+    return (
+
+        <div className='max-w-[1640px] mx-auto p-4'>
+            <div className='max-h-[500px] relative'>
+                {/* overlay  */}
+                <div className=' absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
+                    <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span className=' text-sky-500'>Best</span></h1>
+                    <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'> <span className=' text-sky-500'>Foods</span>Delivered</h1>
+                </div>
+                <img className='w-full max-h-[500px] object-cover' src="https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80  " alt="" />
+            </div>
+        </div>
+    )
 }
 
 export default Hero
