edbc0b80d888b3ccb454fdb51a6abd0bd2b4a37e
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-23 21:57:02 +0600

    headline Cards section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
 
 function App() {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <HeadlineCards/>
     </>
   )
 }
---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function HeadlineCards() {
+    return (
+        <div>
+            {/* card  */}
+            <div>
+                {/* overly  */}
+                <div>
+                <p>Sun's Out, BOGO's Out</p>
+                <p>Through 8/26</p>
+                <button>Order Now</button>
+                </div>
+                <img src="https://images.unsplash.com/photo-1529692236671-f1f6cf9683ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default HeadlineCards
