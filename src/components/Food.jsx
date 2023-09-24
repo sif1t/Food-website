4742f894a7eb86aa6dcf22790732d24b0cf7ef68
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-24 16:52:26 +0600

    top rated menu items foods card coplite

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { data } from '../data/data.js'
 const Food = () => {
+    const [foods, setFoods] =useState(data)
+
+
     return (
         <div className='max-w-[1640px] m-auto px-4 py-12'>
            <h1 className='text-sky-500 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
@@ -31,11 +34,19 @@ const Food = () => {
            </div>
 
         {/* display food  */}
-       <div>
-        
+       <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+           {foods.map((item, index)=> (
+              <div key={index} className='border shadow-lg rounded-lg hover:scale-105 duration-300'>
+                <img src={item.image} alt={item.name} className='w-full h-[200px] object-cover rounded-lg' />
+                <div className='flex justify-between px-2 py-4'>
+                    <p className='font-bold'>{item.name}</p>
+                    <p>
+                        <span className='bg-sky-600 text-white p-1 rounded-full'>{item.price}</span>
+                    </p>
+                </div>
+              </div>
+           ) )}
        </div>
-
-
 
         </div>
     )
