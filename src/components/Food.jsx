0ce703cee8058ca6feea46e6134cd8dd1278c5a3
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-24 15:33:43 +0600

    add filter row section

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import { data } from '../data/data.js'
 const Food = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+           <h1>Top Rated Menu Items</h1>
+
+           {/* filter row  */}
+           <div>
+            
+           </div>
+        </div>
+    )
 }
 
 export default Food
